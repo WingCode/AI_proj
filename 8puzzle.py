ad650cec9d180555e16b8c8935ae239f1d1236f6
smallstar1234 <smallstar1234@gmail.com>
2017-02-02 14:34:33 +0530

    Add files via upload
    Have to fix the problem of current_board state is getting changed in local variable of current_state_2

--- 8puzzle.py
+++ 8puzzle.py
@@ -1,7 +1,6 @@
 from sets import Set
 
 goal_state=[[1,2,3],[8,0,4],[7,6,5]]
-current_state=[[2,8,3],[1,6,4],[7,0,5]]
 depth=3
 states_set=Set()
 goal_set=Set([123804765])
@@ -52,7 +51,7 @@ def no_moves(i,j):
 
 	return move
 
-def find_blank():
+def find_blank(current_state=[]):
 	i_=0
 	j_=0
 	for i in range(0,3):
@@ -63,7 +62,7 @@ def find_blank():
 				break
 	return i_,j_
 
-def heu(current_state):
+def heu(current_state=[]):
 	out_place=0
 	for i in range(0,3):
 		for j in range(0,3):
@@ -75,53 +74,78 @@ def heu(current_state):
 def swap(one,two):
 	return two,one
 
-def mover2(i_,j_,movement,current_state2=[]):
-	print movement,current_state2
+def mover2(i_,j_,movement,current_state=[]):
+	#print movement,"crst"
+	#print_board(current_state)
+	current_state2_=current_state[:]
 	if movement==0:
-		current_state2[i_][j_],current_state2[i_-1][j_]=swap(current_state2[i_][j_],current_state2[i_-1][j_])
+		current_state2_[i_][j_],current_state2_[i_-1][j_]=swap(current_state2_[i_][j_],current_state2_[i_-1][j_])
 
 	elif movement==1:
-		current_state2[i_][j_],current_state2[i_][j_+1]=swap(current_state2[i_][j_],current_state2[i_][j_+1])
+		current_state2_[i_][j_],current_state2_[i_][j_+1]=swap(current_state2_[i_][j_],current_state2_[i_][j_+1])
 
 	elif movement==2:
-		current_state2[i_][j_],current_state2[i_+1][j_]=swap(current_state2[i_][j_],current_state2[i_+1][j_])
+		current_state2_[i_][j_],current_state2_[i_+1][j_]=swap(current_state2_[i_][j_],current_state2_[i_+1][j_])
 
 	elif movement==3:
-		current_state2[i_][j_],current_state2[i_][j_-1]=swap(current_state2[i_][j_],current_state2[i_][j_-1])
+		current_state2_[i_][j_],current_state2_[i_][j_-1]=swap(current_state2_[i_][j_],current_state2_[i_][j_-1])
 
-	print current_state2
+	print_board(current_state2_)
 
 	multipler=100000000
 	sum_=0
 	for i in range(0,3):
 		for j in range(0,3):
-			sum_=sum_+current_state[i][j]*multipler
+			sum_=sum_+current_state2_[i][j]*multipler
 			multipler=multipler/10
 
 	if sum_ in states_set:
+		return None
+
+	else:
 		states_set.add(sum_)
 
-	value=heu(current_state)
-	return value,current_state
+	print("The recorded states are:",states_set)
+	value=heu(current_state2_)
+	return value,current_state2_
 
-def mover(i_,j_,iterator,move_list=[]):
+def mover(i_,j_,iterator,move_list=[],current_state=[]):
 
 	h_x=('Inf')
 	best_state=[]
 	for i in range(0,len(move_list)):
+		#print "IN LOOP"
+		#print_board(current_state)
 		value,matrix=mover2(i_,j_,move_list[i],current_state)
 		
 		if value<h_x:
 			h_x=value
 			best_state=matrix
 
+	print("The current depth level %d and heurestic value %d"%(iterator,h_x))
 	return best_state
 
+def print_board(current_state=[]):
+	for i in range(0,3):
+		for j in range(0,3):
+			if current_state[i][j]==0:
+				print "   ","|",
 
+			else:
+				print current_state[i][j],"|",
+
+		print("\n")
+		if i!=2:
+			print("---------------")
+	print("\n\n")
+
+current_state=[[2,8,3],[1,6,4],[7,0,5]]
 iterator=0
+print "The intial configuration"
+print_board(current_state)
 while iterator<depth:
-	i_,j_=find_blank()
+	i_,j_=find_blank(current_state)
 	move_list=no_moves(i_,j_)
-	current_state=mover(i_,j_,iterator,move_list)
+	current_state=mover(i_,j_,iterator,move_list,current_state)
 
 	iterator+=1
